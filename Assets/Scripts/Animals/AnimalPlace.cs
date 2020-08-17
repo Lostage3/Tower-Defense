743e9f2dc916ff9409f653245e6ddf3b640a6c89
Lostage3 <lostage30030@gmail.com>
2020-08-17 14:04:15 +0200

    GameStates

--- Assets/Scripts/Animals/AnimalPlace.cs
+++ Assets/Scripts/Animals/AnimalPlace.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 public class AnimalPlace : MonoBehaviour
 {
     public GameObject AnimalPrefab;
-
+    GameObject animal;
+    bool animalBool;
+    private void Start()
+    {
+        animalBool = false;
+    }
     private void OnMouseUpAsButton()
     {
-        GameObject animal = Instantiate(AnimalPrefab,new Vector3(transform.position.x, transform.position.y + 2.5f, transform.position.z),Quaternion.identity);
-        //animal.transform.position += new Vector3(transform.position.x,transform.position.y + 5f,transform.position.z);
+        if (animalBool == false)
+        {
+            animal = Instantiate(AnimalPrefab, new Vector3(transform.position.x, transform.position.y + 2.5f, transform.position.z), Quaternion.identity);
+            //animal.transform.position += new Vector3(transform.position.x,transform.position.y + 5f,transform.position.z);
+            animalBool = true;
+
+        }
+       
     }
 }
---
+++ Assets/Scripts/Game/GameController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public bool IsNewGame;
+    public bool IsContinue;
+    public bool IsExit;
+    public bool IsLevelOnePreparation;
+    public bool IsReturnToMenu;
+    public bool IsLevelOne;
+    public bool IsReturnToOverWorld;
+    public bool IsReturnToPreparation;
+
+    
+    void OnMouseUp()
+    {
+        if (IsNewGame)
+        {
+            SceneManager.LoadScene("OverWorld",LoadSceneMode.Single);
+        }
+        if (IsContinue)
+        {
+            SceneManager.LoadScene("OverWorld", LoadSceneMode.Single);
+        }
+        if (IsExit)
+        {
+            Application.Quit();
+        }
+        if (IsLevelOnePreparation)
+        {
+            SceneManager.LoadScene("Preparation", LoadSceneMode.Single);
+        }
+        if (IsReturnToMenu)
+        {
+            SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+        }
+        if (IsLevelOne)
+        {
+            SceneManager.LoadScene("Tower Defense", LoadSceneMode.Single);
+        }
+        if (IsReturnToOverWorld)
+        {
+            SceneManager.LoadScene("OverWorld", LoadSceneMode.Single);
+        }
+        if (IsReturnToPreparation)
+        {
+            SceneManager.LoadScene("Preparation", LoadSceneMode.Single);
+        }
+    }
+}
